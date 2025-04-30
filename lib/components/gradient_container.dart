e6c96b7e6101a2265f1bfdaf9f4863bdb469f939
Balnur Bakiram <balnurbakiram@Macbook-Pro-14-M3-Balnur.local>
2025-04-30 14:37:00 +0500

    first commit

---
+++ lib/components/gradient_container.dart
@@ -0,0 +1,28 @@
+// ignore: depend_on_referenced_packages
+import 'package:dice_roller/components/dice_roller.dart';
+import 'package:flutter/material.dart';
+import 'package:rolldice/components/dice_roller.dart';
+
+const startAlignment = Alignment.topLeft;
+const endAlignment = Alignment.bottomRight;
+
+class GradientContainer extends StatelessWidget {
+  const GradientContainer(this.color1, this.color2, {super.key});
+
+  final Color color1;
+  final Color color2;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        gradient: LinearGradient(
+          begin: startAlignment,
+          end: endAlignment,
+          colors: [color1, color2],
+        ),
+      ),
+      child: const Center(child: DiceRoller()),
+    );
+  }
+}
---
+++ lib/main.dart
@@ -0,0 +1,19 @@
+import 'package:dice_roll/components/gradient_container.dart';
+import 'package:flutter/material.dart';
+import 'package:rolldice/components/gradient_container.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      home: Scaffold(body: GradientContainer(Colors.purple, Colors.deepPurple)),
+    );
+  }
+}
